dfba3409aaf5ad38c5802fa56f13291eb9c526cb
vonbarown <vonielbrown@pursuit.org>
2019-07-19 16:16:15 -0400

    completed answers to callback lab

--- callback_lab.js
+++ callback_lab.js
@@ -18,29 +18,22 @@ function forEachElem (eachArray, arrayCall){
 
 //console.log(forEachElem(arrEachElem,arrPrint));
 
-console.log( `\n 2. Create a function logEachElem that takes an array as an argument, and uses the 'forEachElem' method to log each element of the array.\n`);
+console.log( `\nproblem 2. Create a function logEachElem that takes an array as an argument, and uses the 'forEachElem' method to log each element of the array.\n`);
 let num2Array =[3,6,9,12,34]
 
 const logEachElem =(logArr) => {
   for(let i =0; i < logArr.length; i++){
     console.log(logArr[i]);
   }
-}
+};
 
 // using the forEachElem fucntion to call the logEachElem function to log the array elements
 forEachElem(num2Array,logEachElem);
 
-console.log(`\n3. Create a function 'logArrTypes' that uses the 'forEachElem' method to log each array element followed by its type.\n`); //For example:
+console.log(`\nProblem 3. Create a function 'logArrTypes' that uses the 'forEachElem' method to log each array element followed by its type.\n`); //For example:
 //
 // ```js
-let mixedArr = [1, 'cat', true]
-// logArrTypes(mixedArr)
-// // will log:
-// // 1 is a number
-// // 'cat' is a string
-// // true is a boolean
-//
-// ```
+let mixedArr = [1, 'cat', true, null, 'str', 30];
 // the function that will be called by the forEachElem fucntion, that loops into the array
 function logArrTypes(typeofArr) {
   let type ='';
@@ -50,60 +43,54 @@ function logArrTypes(typeofArr) {
   }
 }
 // using the forEachElem fucntion to call the logEachElem function to log the array elements
-// and typeof elemen
+// and typeof element
 forEachElem(mixedArr,logArrTypes);
 
 //4. Write a function 'myMap' that takes in an array and a callback. Call the callback on each element of the array and add the result
 //to an output array. Return the output array.
-
 const myMap =(mapArr,mapCallback) => {
+  //console.log(mapArr);
   let arrMap =[];
-  for (let i =0; i < mapArr.length; i++){
+  for (let i =0; i<mapArr.length;i++){
     arrMap.push(mapCallback(mapArr[i]));
   }
-  return arrMap
-}
+  return arrMap;
+};
 
-console.log(`\n5. Create a function 'allCaps' that takes in an array as an argument, and uses the 'myMap' function to return all the elements capitalized.\n`);
+console.log(`\nProblem 5. Create a function 'allCaps' that takes in an array as an argument, and uses the 'myMap' function to return all the elements capitalized.\n`);
 
 let lowCase = ['this ','is ','my ','array ', 'for ', 'question ', 'five'];
 const allCaps = (capsArray) => {
-  let upCase =[];
+  let upCase ='';
   for (let i =0; i < capsArray.length; i++){
-     capsArray[i].toUpperCase();
+   upCase += capsArray[i].toUpperCase();
   }
-  return capsArray;
+  //console.log(upCase);
+  return upCase;
+};
+console.log(myMap(lowCase,allCaps));
+
+//6. You are given the following functions:
+console.log('\nQuestion #6');
+
+function conservativeSpender(balance)  {
+  return balance > 100;
 }
 
-console.log(myMap(lowCase,allCaps));
-// 6. You are given the following functions:
-// 'js
-//
-// function conservativeSpender(balance)  {
-//   return balance > 100
-// }
-//
-// function liberalSpender(balance) {
-//   return balance > 10
-// }
-//
-// function horribleSaver (balance) {
-//   return balance > 0
-// }
-//
-// '
-//
+function liberalSpender(balance) {
+  return balance > 10;
+}
+
+function horribleSaver (balance) {
+  return balance > 0;
+}
 // Write a function 'shouldIBuyThis' that takes in a balance and a callback (one of the above functions). The fuction should return either
 // "Sure! I've got the money!" or "Nope! Gotta keep my savings up!"
-//
-// 'js
-// console.log(shouldIBuyThis(20, horribleSaver))
-// // logs: "Sure! I've got the money!"
-// console.log(shouldIBuyThis(20, liberalSpender))
-// // logs: "Sure! I've got the money!"
-// console.log(shouldIBuyThis(20, conservativeSpender))
-// // logs:  "Nope! Gotta keep my savings up!"
-// console.log(shouldIBuyThis(101, conservativeSpender))
-// // logs: "Sure! I've got the money!"
-//
-// '
+const shouldIBuyThis = (balance,callback) => {
+  if (callback(balance)){
+    return "Sure! I've got the money!";
+  } else {
+    return "Nope! Gotta keep my savings up!";
+  }
+};
+console.log(shouldIBuyThis(99, conservativeSpender));

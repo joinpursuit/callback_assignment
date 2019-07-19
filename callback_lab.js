4ee4b690c295d639f5e6887a1c7c97fb8e93209b
vonbarown <vonielbrown@pursuit.org>
2019-07-19 09:29:04 -0400

    still working

--- callback_lab.js
+++ callback_lab.js
@@ -1,21 +1,22 @@
 // # Callback Exercises
 //
-console.log(`1. Write a function 'forEachElem' that takes in an array and a callback. Call the callback on each element in the array.\n`);
-let arrEachElem = ['This','is','an', 'array']
+//1. Write a function 'forEachElem' that takes in an array and a callback. Call the callback on each element in the array.\n`);
 
-function arrPrint (inar) {
-  let print=[];
-  for (let i=0; i < inar.length; i++) {
-    print.push(inar[i]);
-  }
-  return print
-}
+//let arrEachElem = ['This','is','an', 'array']
+// function arrPrint (inar) {
+//   let print=[];
+//   for (let i=0; i < inar.length; i++) {
+//     print.push(inar[i]);
+//   }
+//   return print
+// }
 
+//creating the function that has a callback as a paramter and return
 function forEachElem (eachArray, arrayCall){
   return arrayCall(eachArray);
 }
 
-console.log(forEachElem(arrEachElem,arrPrint));
+//console.log(forEachElem(arrEachElem,arrPrint));
 
 console.log( `\n 2. Create a function logEachElem that takes an array as an argument, and uses the 'forEachElem' method to log each element of the array.\n`);
 let num2Array =[3,6,9,12,34]
@@ -26,6 +27,7 @@ const logEachElem =(logArr) => {
   }
 }
 
+// using the forEachElem fucntion to call the logEachElem function to log the array elements
 forEachElem(num2Array,logEachElem);
 
 console.log(`\n3. Create a function 'logArrTypes' that uses the 'forEachElem' method to log each array element followed by its type.\n`); //For example:
@@ -39,6 +41,7 @@ let mixedArr = [1, 'cat', true]
 // // true is a boolean
 //
 // ```
+// the function that will be called by the forEachElem fucntion, that loops into the array
 function logArrTypes(typeofArr) {
   let type ='';
   for (let i =0; i < typeofArr.length; i++) {
@@ -46,17 +49,33 @@ function logArrTypes(typeofArr) {
     console.log(`${typeofArr[i]} is a ${type}`);
   }
 }
-
+// using the forEachElem fucntion to call the logEachElem function to log the array elements
+// and typeof elemen
 forEachElem(mixedArr,logArrTypes);
 
-console.log(`\n4. Write a function 'myMap' that takes in an array and a callback. Call the callback on each element of the array and add the result
-to an output array. Return the output array.\n`);
+//4. Write a function 'myMap' that takes in an array and a callback. Call the callback on each element of the array and add the result
+//to an output array. Return the output array.
 
 const myMap =(mapArr,mapCallback) => {
-  return mapCallback(),
+  let arrMap =[];
+  for (let i =0; i < mapArr.length; i++){
+    arrMap.push(mapCallback(mapArr[i]));
+  }
+  return arrMap
 }
 
-// 5. Create a function 'allCaps' that takes in an array as an argument, and uses the 'myMap' function to return all the elements capitalized.
+console.log(`\n5. Create a function 'allCaps' that takes in an array as an argument, and uses the 'myMap' function to return all the elements capitalized.\n`);
+
+let lowCase = ['this ','is ','my ','array ', 'for ', 'question ', 'five'];
+const allCaps = (capsArray) => {
+  let upCase =[];
+  for (let i =0; i < capsArray.length; i++){
+     capsArray[i].toUpperCase();
+  }
+  return capsArray;
+}
+
+console.log(myMap(lowCase,allCaps));
 // 6. You are given the following functions:
 // 'js
 //

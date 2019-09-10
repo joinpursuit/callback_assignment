226e059b298fedd9e0dbab456cccd4a185a98a1b
tawnyblvd <margara.sees@gmail.com>
2019-09-10 15:51:53 -0400

    update for q6a

--- callbacks.js
+++ callbacks.js
@@ -234,18 +234,33 @@ function logArrTypes(arr, callback) {
 
 console.log(logArrTypes(typesArr, forEachElem));
 
+console.log("______________");
+
 // ## Question Six
 //
-// a. Write a function called `myMap` that takes in an array and a callback as arguments.  It should return a new array with the callback applied to each element in the array.
+// a. Write a function called `myMap` that takes in an array and a callback as arguments.
+//It should return a new array with the callback applied to each element in the array.
 //
 // ```
 // Input: [3,5,6], function(num) { return num * 3 }
 //
 // Output: [9, 15, 18]
 // ```
+function myMap(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+  let newMappy = myNextCallback(arr[i]);
+  console.log(newMappy);
+  }
+}
+
+function myNextCallback(num) {
+    return num*3;
+  }
+
+myMap([3,5,6]);
 //
-//
-// b. Create a function `allCaps` that takes in an array as an argument, and uses the `myMap` function to return all the elements capitalized.
+// b. Create a function `allCaps` that takes in an array as an argument, and uses the `myMap`
+//function to return all the elements capitalized.
 //
 // ```
 // Input: ["The", "internet", "is", "not", "a", "big", "truck.", "It's", "a", "series", "of", "tubes"]

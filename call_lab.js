4b5c3d744455fd0542a09dfdac132af303ee8260
dannyl <danilovlaurent@pursuit.org>
2019-10-15 21:59:43 -0400

    problems 1-4

--- call_lab.js
+++ call_lab.js
@@ -1,31 +1,31 @@
-console.log("Question 1")
+// console.log("Question 1")
 
-const callHello = (cb1) =>{
-    return cb1
-}
-callHello(console.log("hello"))
+// const callHello = (cb1) =>{
+//     return cb1
+// }
+// callHello(console.log("hello"))
 
-console.log("Question 2")
+// console.log("Question 2")
 
-const cbBoo = (bl) =>{
-    if (bl ===  true){
-       return console.log(bl)
-    }
-    else {
-       console.log("Ignoring the callback")
-    }
-}
-console.log(cbBoo(0))
+// const cbBoo = (bl) =>{
+//     if (bl ===  true){
+//        return console.log(bl)
+//     }
+//     else {
+//        console.log("Ignoring the callback")
+//     }
+// }
+// console.log(cbBoo(0))
 
 
 
-console.log("Question 3")
-const playMusic = (cb2, num) =>{
-    for(let i =1; i<=num; i++){
-        console.log(cb2);
-    }
-}
-console.log(playMusic("Rock", 4))
+// console.log("Question 3")
+// const playMusic = (cb2, num) =>{
+//     for(let i =1; i<=num; i++){
+//         console.log(cb2);
+//     }
+// }
+// console.log(playMusic("Rock", 4))
 
 
 console.log("Question 4")

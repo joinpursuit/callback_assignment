235a4e140ef6431bb37fc3c8fa91ccfe8f703b7f
Danielle R Cherry <daniellecherry@pursuit.org>
2019-10-15 22:01:58 -0400

    Solve Problem 2

--- callback_assigment.js
+++ callback_assigment.js
@@ -10,10 +10,42 @@
 // Logs "Hello!" to the console
 
 
+// const callback = (fn) => {
+//     return (fn)
+// }
+// const takeCallback = (cb) => {
+//     return ('Hello!')
+// } 
+// console.log(takeCallback(callback))
+
+
+// ## Question Two
+
+// Write a function that takes in a callback and a boolean.  
+// If the boolean is true, call the callback, 
+// otherwise log "Ignoring the callback" to the console.
+
+// ```
+// Input: () => console.log(""), true
+
+// Output:
+// Logs "" to the console
+
+// Input: () => console.log("Testing"), false
+// Output:
+// Logs "Ignoring the callback" to the console.
+// ```
+
 const callback = (fn) => {
     return (fn)
 }
-const takeCallback = (cb) => {
-    return ('Hello!')
-} 
-console.log(takeCallback(callback))
+
+const booleanCb = (cb, boolean) => {
+    if(boolean) {
+         callback
+    } else {
+        console.log("Ignoring the callback")
+    }
+}
+
+console.log(callback("Testing"))

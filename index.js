ddb1efd834e1fb2e89f5b548ed14eb18c381ef9f
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-15 21:50:33 -0400

    complete problem 2 && 3

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const caller = (fn) => {
     return fn();
 }
 
-console.log(caller(() => "Hello"))
+// console.log(caller(() => "Hello"))
 
 //PROBLEM 2
+const booleanCheck = (boolean, fn) => {
+    if(boolean === true) {
+       return fn();
+    } else {
+        return "ignoring the callback";
+    }
+}
+
+// console.log(booleanCheck(true, () => "Hello"));
+
+//PROBLEM 3
+const callNumber = (num, fn) => {
+    for(let i = 1; i <= num; i++) {
+        console.log(fn())
+    }
+}
+
+// callNumber(2, () => "Hello")
+
+//PROBLEM 4

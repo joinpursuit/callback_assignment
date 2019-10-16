2bde62efce82a18ab9717877fbb077cf4a76df6a
kongnyc <54258089+kongnyc@users.noreply.github.com>
2019-10-16 01:08:09 -0400

    solve problem 4

--- call_lab.js
+++ call_lab.js
@@ -1,23 +1,23 @@
-console.log("Question 1")
+// console.log("Question 1")
 
-const callHello = (cb1) =>{
-    cb1 = cb1
-}
+// const callHello = (cb1) =>{
+//     cb1 = cb1
+// }
 
-callHello(console.log("Hello!"))
+// callHello(console.log("Hello!"))
 
-console.log("Question 2")
+// console.log("Question 2")
 
-const cbBoo = (b1,boo) =>{
-    if (boo ===  true){
-       b1();
-    }
-    else {
-       console.log("Ignoring the callback")
-    }
-}
-const mySta = () => {console.log("Im true")}
-console.log(cbBoo(mySta, false))
+// const cbBoo = (b1,boo) =>{
+//     if (boo ===  true){
+//        b1();
+//     }
+//     else {
+//        console.log("Ignoring the callback")
+//     }
+// }
+// const mySta = () => {console.log("Im true")}
+// console.log(cbBoo(mySta, false))
 
 // console.log("Question 3")
 // const playMusic = (cb2, num) =>{
@@ -28,4 +28,13 @@ console.log(cbBoo(mySta, false))
 // console.log(playMusic("Rock", 4))
 
 
-// console.log("Question 4")
+console.log("Question 4")
+
+const finResult = (num, cb1,cb2) =>{
+        cb1(num);
+      return cb2(cb1(num))
+}
+
+const add3 = (num)=>{return num+3};
+const mut2 = (num)=>{return num*2};
+console.log(finResult(4,add3,mut2))

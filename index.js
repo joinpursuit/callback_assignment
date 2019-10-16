beb1d143b053a481aed9f14576b12b381244afdf
Brandon Brown <brandonb0206@gmail.com>
2019-10-15 21:57:11 -0400

    Question two

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 
 // Write a function that takes in a callback and calls the callback
 
-const newFunction = (callback) => {return callback}
+// const newFunction = (callback) => {return callback}
 
-console.log(newFunction("Hello"))
+// console.log(newFunction("Hello"))
+
+// Question two
+
+const secondFunc = (callback, boolean) => {
+    if(boolean === true ){
+        return callback
+    }else {
+        return "Ignoring the callback" 
+    }
+}
+
+console.log(secondFunc("B", true))
+

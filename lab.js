ca0452088c40cff66e21207cd82c95bac09f3168
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-10-15 22:01:18 -0400

    draft

---
+++ lab.js
@@ -0,0 +1,30 @@
+// Question 1 
+
+const sayHello = () => {
+    return "Hello"
+} 
+const greeting = (fn) => {
+    return fn ()
+}
+console.log(greeting(sayHello))
+
+// Question 2 
+
+const input = (bool, callBack) => {
+    if(bool === true) {
+        return callBack()
+    } else {
+        return "ignoring the callback"
+    }
+    
+}
+
+console.log
+    
+    input(true, ( () => console.log(" ") )))
+
+// Question 3
+
+// const callNum = (num, cb) => {
+
+// }

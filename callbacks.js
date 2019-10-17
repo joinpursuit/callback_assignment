75c2317bfae5790792ccf148d7c80801dd7abbf6
Cassidy Beni <cassidybeni@pursuit.org>
2019-10-16 22:00:39 -0400

    completed lab

--- callbacks.js
+++ callbacks.js
@@ -75,8 +75,59 @@ const forEachElem = (arr, callback) => {
 // logEachElem([1,2,34]);
 
 //Question 6c
-const logArrTypes = (arr) => {
-    forEachElem(arr, (i) => console.log(`${i} is a ${typeof(i)}`));
-}
-logArrTypes([1, "Hi", true, {color: "green"}]);
+// const logArrTypes = (arr) => {
+//     forEachElem(arr, (i) => console.log(`${i} is a ${typeof(i)}`));
+// }
+// logArrTypes([1, "Hi", true, {color: "green"}]);
 
+//Question 7a
+
+// const myMap = (arr, callback) => {
+//     let emptyArray = []
+//     for(let i = 0; i < arr.length; i++) {
+//         emptyArray.push(callback(arr[i]))
+//     } return emptyArray
+// }
+// console.log(myMap([3,5,6], function(num) { return num * 3 }))
+
+// //Question 7b
+
+// const allCaps = (arr) => {
+//     return myMap(arr, i => i.toUpperCase())
+// }
+//     console.log(allCaps(["The", "internet", "is", "not", "a", "big", "truck.", "It's", "a", "series", "of", "tubes"]))
+
+//Question 8a
+const myFilter = (arr, callback) => {
+    let newArray = []
+    for(let i = 0; i< arr.length; i++){
+        if(callback(arr[i])){
+            newArray.push(arr[i])
+        }
+    } return newArray
+}
+//console.log(myFilter([1,4,6,7, -3], num => num < 5))
+
+//Question 8b
+
+const removeEvenNumbers = (array) => {
+    return myFilter(array, num => {
+    if(num % 2 === 0){
+        return true
+    } else {
+        return false
+    }})
+}
+    
+console.log(removeEvenNumbers([1,2,3,4,5,6,7,8,9]))
+
+//Question 8c
+const removeShortWords = (arr) => {
+    return myFilter(arr, (i) => {
+    if(i.length > 4 ){
+        return true
+    } else {
+        return false
+    }})
+}
+console.log(removeShortWords(["JavaScript", "is", "a", "high-level,", "interpreted", "programming", "language", "that", "conforms", "to", "the", "ECMAScript", "specification"]))

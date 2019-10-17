f5093e343687fad9ac741de69bb531d07da1e66d
david maravillas <davidmaravillas@pursuit.org>
2019-10-16 20:32:51 -0400

    question 4 complete

--- lab.js
+++ lab.js
@@ -1,30 +1,36 @@
-// Question 1 
+// // Question 1 
 
-const sayHello = () => {
-    return "Hello"
-} 
-const greeting = (fn) => {
-    return fn ()
-}
-console.log(greeting(sayHello))
+// const sayHello = () => {
+//     return "Hello"
+// } 
+// const greeting = (fn) => {
+//     return fn ()
+// }
+// console.log(greeting(sayHello))
 
-// Question 2 
+// // Question 2 
 
-const input = (bool, callBack) => {
-    if(bool === true) {
-        return callBack()
-    } else {
-        return "ignoring the callback"
-    }
+// const input = (bool, callBack) => {
+//     if(bool === true) {
+//         return callBack()
+//     } else {
+//         return "ignoring the callback"
+//     }
     
-}
+// }
 
-console.log
+// console.log
     
-    input(true, ( () => console.log(" ") )))
+//     input(true, ( () => console.log(" ") )))
 
 // Question 3
 
-// const callNum = (num, cb) => {
+const repeat = (num,fn) =>{
+    let counter = 1
+    while(counter <= num ){
+        fn()
+        counter += 1
+    }
+}
 
-// }
+repeat(4,() => console.log(`I'm being called`))

7a6998a0a731f66eb761cfeb8dd2496ad6f9a1e5
david maravillas <davidmaravillas@pursuit.org>
2019-10-16 21:45:54 -0400

    Q6a complete

--- lab.js
+++ lab.js
@@ -25,12 +25,57 @@
 
 // Question 3
 
-const repeat = (num,fn) =>{
-    let counter = 1
-    while(counter <= num ){
-        fn()
-        counter += 1
+// const repeat = (num,fn) =>{
+//     let counter = 1
+//     while(counter <= num ){
+//         fn()
+//         counter += 1
+//     }
+// }
+
+// repeat(4,() => console.log(`I'm being called`))
+
+// // question 4
+
+// const doubleCall = (num, funOne, funTwo) =>{
+//     return  funTwo(funOne(num))
+// }
+
+// console.log(doubleCall(4, (num) => num + 3, (num) => num * 2))
+
+
+// question 5
+
+// function conservativeSpender(balance)  {
+//     return balance > 100
+//   }
+  
+//   function liberalSpender(balance) {
+//     return balance > 10
+//   }
+  
+//   function horribleSaver (balance) {
+//     return balance > 0
+//   }
+  
+
+// const shouldIBuythis = (num,fun) =>{
+//     if( 20 > 0 ){
+//         return "Sure! I've got the money!"
+//     } else {
+//         return "Nope! Gotta keep my savings up!"
+    
+// }
+// }
+
+// console.log(shouldIBuyThis(20,horribleSaver))
+
+// question six
+
+const forEachElem = (arr,callBack) => {
+    for (let i = 0; i < arr.length; i += 1) {
+        callBack(arr[i])
     }
 }
 
-repeat(4,() => console.log(`I'm being called`))
+forEachElem([1,4,5,6], (elem) => console.log("$"+ elem))

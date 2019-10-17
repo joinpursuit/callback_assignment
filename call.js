586d45a45769be2e07b7acd770a1fc593bc4c32f
rafidhossain1995 <rafidhossain@pursuit.org>
2019-10-16 21:57:10 -0400

    question 4

--- call.js
+++ call.js
@@ -45,3 +45,44 @@
 //     return num * 2
 // }
 // console.log(result(completeCallBack, finalCallBack, 9))
+
+
+// function conservativeSpender(balance)  {
+//     return balance > 100
+//   }
+  
+//   function liberalSpender(balance) {
+//     return balance > 10
+//   }
+  
+//   function horribleSaver (balance) {
+//     return balance > 0
+//   }
+  
+//   const shouldIBuyThis = (num, callback) => {
+//     if (callback(num)){
+//         return "Sure! Ive got the money!"
+//     } else {
+//         return "Nope! Gotta keep my savings"
+//     }
+//   }
+//   console.log(shouldIBuyThis(103, conservativeSpender))
+
+const forEachElem = (arr, callback) => {
+    let num = 0
+    for (i = 0; i < arr.length; i++){
+        num = callback(arr[i])
+    }
+    //return num
+}
+// const realElem = (num) => {
+//       console.log(num)
+// }
+// console.log(forEachElem([1,5, 6, 7], realElem))
+
+const logEachElem = (arr) => {
+    let num =0;
+    forEachElem(arr, (elem) => num = elem)
+    console.log (num)
+}
+console.log(logEachElem([1, 4, 5, 6]))

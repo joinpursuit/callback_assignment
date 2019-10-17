cb0bd1935605597d0828f5b50edd757f42cf507f
trilltown <phil.awich@gmail.com>
2019-10-16 20:39:40 -0400

    Solve Problem 3

--- index.js
+++ index.js
@@ -8,13 +8,33 @@
 
 // Question two
 
-const secondFunc = (callback, boolean) => {
-    if(boolean === true ){
-        return callback
-    }else {
-        return "Ignoring the callback" 
+// const secondFunc = (callback, boolean) => {
+//     if(boolean === true ){
+//         return callback
+//     }else {
+//         return "Ignoring the callback" 
+//     }
+// }
+
+// console.log(secondFunc("B", false))
+
+// ## Question Three
+
+// Write a function that takes in a callback and a number.  It should call the callback that many times.
+// ```
+
+const thirdFunc = (callback, number) => {
+    let i = 1
+    while (i <= number) {
+        console.log(callback)
+        i++
     }
 }
+thirdFunc("Hello World", 2)
 
-console.log(secondFunc("B", false))
-
+const thirdFunc = (callback, number) => {
+    for (let i = 0; i < number ; i++) {
+        return callback
+    }
+}
+console.log(thirdFunc("hello world", 2))

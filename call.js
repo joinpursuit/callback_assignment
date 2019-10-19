59420261c584e4250c1f896c08d6827374a53163
rafidhossain1995 <rafidhossain@pursuit.org>
2019-10-18 23:50:26 -0400

    question 8

--- call.js
+++ call.js
@@ -18,20 +18,20 @@
 // }
 // console.log(boo1(true, callback))
 
-// const called = (callback, number) => {
-//         return callback * 
+//Question 3
+// const called = (callback, num) => {
+//        let str = callback
+//        for(let i =0; i < num; i++ ){
+//         console.log(str)
+//        } 
+//        return str
 //     }
-//     console.log(called("Im being called"))
+//     console.log(called("Im being called", 4))
 
-// const multi = (number) => {
-//     for (i = 0; i <= number; i++){
-//     called(callback (string) => string * number
-//     return multi
-//     }
-// }
+
 // console.log(called())
 
-
+// Question 4
 // const result = (callback1, callback2, num) => {
         
 //         num = callback1(num)
@@ -68,21 +68,86 @@
 //   }
 //   console.log(shouldIBuyThis(103, conservativeSpender))
 
+//Question 6
+
 const forEachElem = (arr, callback) => {
     let num = 0
     for (i = 0; i < arr.length; i++){
-        num = callback(arr[i])
+        num = callback(arr[i]) //Set the value of num to what ever is returned in the callback(which could be any of the below functions)
     }
-    //return num
 }
-// const realElem = (num) => {
-//       console.log(num)
+// const realElem = (num) => {   //Takes in the arr value from the for each array(arr) on line 76
+//       return console.log(num)   // returns the arr value from the for each array(arr) on line 76
 // }
-// console.log(forEachElem([1,5, 6, 7], realElem))
+// console.log(forEachElem([1,5, 6, 7], realElem)) // runs the forEach function with an array and the callback function realElem
 
-const logEachElem = (arr) => {
-    let num =0;
-    forEachElem(arr, (elem) => num = elem)
-    console.log (num)
+//6B
+// const logEachElem = (arr) => {
+//       forEachElem(arr, (elem) =>  console.log(elem) ) //// runs the forEach function with an array and the callback function that is created called elem
+//       return ""
+// }
+// console.log(logEachElem([1,2,34]))
+
+//6C
+// const logArrTypes = (arr) => {
+//     forEachElem(arr, (elem) =>  console.log(elem + " is a "+typeof(elem)) )
+//     return ""
+// }
+// console.log(logArrTypes([1,"Hi", true, {color: "green"}]))
+
+//Question 7
+// const myMap = (arr, cb) => {
+//     let newArr = []    //Create blank new array
+//     for(let i=0; i < arr.length; i++){
+//        newArr.push(cb(arr[i]))   // push arry values to the new array. and sents that value to the new function(nFunc)
+//     }
+//      return newArr
+// }
+
+// const nFunc = (num) => {
+//         return num * 3;   //Takes value from index from myMapps and the mutiply it by 3
+// }
+// console.log(myMap([3,5,6],nFunc))
+
+// //Question 7 b
+
+// const allCaps = (arr) => {
+
+//     return myMap(arr, (nel) => {   //This runs the myMapp function and creates a new function that returns the uppercase of the values
+//         return nel.toUpperCase()  
+//     })
+// }
+// console.log(allCaps(["The", "internet", "is", "not", "a", "big", "truck.", "It's", "a", "series", "of", "tubes"]))
+
+//Question 8A
+
+// const myFilter = (arr,nFunc) => {
+//     let newArr = []
+//     for(let i =0; i < arr.length; i ++){
+//         newArr.push(nFunc(arr[i]))
+//     }
+//     return newArr
+// }
+
+// const tOnly = (num) =>{
+//    if(num < 5){
+//        return num
+//    }else return ""
+
+// }
+
+// console.log(myFilter([1,4,6,7, -3],tOnly))
+
+//Question 8B
+
+const removeEvenNumbers = (arr) => {
+    let newArr = []
+    for(let i = 0; i <arr.length; i++){
+        if(arr[i] % 2 !== 0){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
 }
-console.log(logEachElem([1, 4, 5, 6]))
+console.log(removeEvenNumbers([1,2,3,4,5,6,7,8,9]))
+

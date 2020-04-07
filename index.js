40eb9b93c2804ea15206f95ff1206cb5a1364433
cassidybeni <cassidybeni@pursuit.org>
2020-04-07 11:17:27 -0400

    Complete problem 2

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ const sayHello = () => {
     console.log("Hello!") 
 }
 questionOne(sayHello)
+
+//Question 2
+
+const questionTwo = (cb, boolean) => {
+    if (boolean) {
+        cb()
+    } else {
+        console.log("Ignoring the callback")
+    }
+}
+
+const runCallback = () => {
+    console.log("Testing")
+}
+questionTwo(runCallback, true)
